d7bb8843f2e7c35ee981596840cd07c028cedecf
David C <45155292+DavoDC@users.noreply.github.com>
2025-03-30 22:24:55 +0800

    Ordered analyser output by variant count
    Least variants at top, most at bottom . Looks better
    Disabled some un-needed stats as well

--- PROJECT/MediaManager/Code/Doer/Analyser/Analyser.cs
+++ PROJECT/MediaManager/Code/Doer/Analyser/Analyser.cs
@@ -16,38 +16,44 @@ namespace MediaManager
             // Notify
             Console.WriteLine("\nAnalysing metadata...");
 
-            // Calculate and print common stats
-            Console.WriteLine("\n - Common statistics:");
-            var extStats = CreateStatList("Extension", f => f.Extension);
-            var yearStats = CreateStatList("ReleaseYear", f => f.ReleaseYear);
-            var formatStats = CreateStatList("CustomFormat", f => f.CustomFormat);
-            var qualityStats = CreateStatList("QualityTitle", f => f.QualityTitle);
-            var videoRangeStats = CreateStatList("VideoDynamicRange", f => f.VideoDynamicRange);
-            var audioCodecStats = CreateStatList("AudioCodec", f => f.AudioCodec);
+            var vidBitStats = new StatList<AnimeFile>("VideoBitDepth", Parser.AnimeFiles, f => f.VideoBitDepth);
+            vidBitStats.Print(0, "(Anime-Specific)");
+
             var audioChannelStats = CreateStatList("AudioChannels", f => f.AudioChannels);
-            var videoCodecStats = CreateStatList("VideoCodec", f => f.VideoCodec);
-            var relGroupStats = CreateStatList("ReleaseGroup", f => f.ReleaseGroup);
-            extStats.Print(0);
-            yearStats.Print(1.0);
-            formatStats.Print(0);
-            qualityStats.Print(0);
-            videoRangeStats.Print(0);
-            audioCodecStats.Print(0);
             audioChannelStats.Print(0);
+
+            var videoCodecStats = CreateStatList("VideoCodec", f => f.VideoCodec);
             videoCodecStats.Print(0);
+
+            var audioLangStats = new StatList<AnimeFile>("AudioLanguages", Parser.AnimeFiles, f => f.AudioLanguages);
+            audioLangStats.Print(0, "(Anime-Specific)");
+
+            var audioCodecStats = CreateStatList("AudioCodec", f => f.AudioCodec);
+            audioCodecStats.Print(0);
+
+            var editionStats = new StatList<MovieFile>("Edition", Parser.MovieFiles, f => f.Edition);
+            editionStats.Print(0, "(Movie-Specific)");
+
+            var yearStats = CreateStatList("ReleaseYear", f => f.ReleaseYear);
+            yearStats.Print(1.0);
+
+            var qualityStats = CreateStatList("QualityTitle", f => f.QualityTitle);
+            qualityStats.Print(0);
+
+            var relGroupStats = CreateStatList("ReleaseGroup", f => f.ReleaseGroup);
             relGroupStats.Print(0.25);
 
-            // Calculate and print movie stats
-            Console.WriteLine("\n - Movie-specific statistics:");
-            var editionStats = new StatList<MovieFile>("Edition", Parser.MovieFiles, f => f.Edition);
-            editionStats.Print(0);
+            // DISABLED BECAUSE 98% unknown
+            //var formatStats = CreateStatList("CustomFormat", f => f.CustomFormat);
+            //formatStats.Print(0);
 
-            // Calculate and print anime stats
-            Console.WriteLine("\n - Anime-specific statistics:");
-            var vidBitStats = new StatList<AnimeFile>("VideoBitDepth", Parser.AnimeFiles, f => f.VideoBitDepth);
-            var audioLangStats = new StatList<AnimeFile>("AudioLanguages", Parser.AnimeFiles, f => f.AudioLanguages);
-            vidBitStats.Print(0);
-            audioLangStats.Print(0);
+            // DISABLED BECAUSE 100% mkv
+            //var extStats = CreateStatList("Extension", f => f.Extension);
+            //extStats.Print(0);
+
+            // DISABLED BECAUSE 100% unknown
+            //var videoRangeStats = CreateStatList("VideoDynamicRange", f => f.VideoDynamicRange);
+            //videoRangeStats.Print(0);
 
             // Finish and print time taken
             Console.WriteLine("");
