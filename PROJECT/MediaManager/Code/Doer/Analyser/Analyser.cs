7414341948f877e5d0fe0bdae53813ef580a71bc
David C <45155292+DavoDC@users.noreply.github.com>
2025-03-30 11:32:19 +0800

    Did stats for all common fields!

--- PROJECT/MediaManager/Code/Doer/Analyser/Analyser.cs
+++ PROJECT/MediaManager/Code/Doer/Analyser/Analyser.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace MediaManager
 {
@@ -16,12 +15,27 @@ namespace MediaManager
             // Notify
             Console.WriteLine("\nAnalysing metadata...");
 
-            //// CALCULATE STATS
-            // Extension stats
+            // Calculate stats
             StatList extStats = new StatList("Extension", Parser.MediaFiles, f => f.Extension);
+            StatList yearStats = new StatList("ReleaseYear", Parser.MediaFiles, f => f.ReleaseYear);
+            StatList formatStats = new StatList("CustomFormat", Parser.MediaFiles, f => f.CustomFormat);
+            StatList qualityStats = new StatList("QualityTitle", Parser.MediaFiles, f => f.QualityTitle);
+            StatList videoRangeStats = new StatList("VideoDynamicRange", Parser.MediaFiles, f => f.VideoDynamicRange);
+            StatList videoCodecStats = new StatList("VideoCodec", Parser.MediaFiles, f => f.VideoCodec);
+            StatList audioCodecStats = new StatList("VideoCodec", Parser.MediaFiles, f => f.AudioCodec);
+            StatList audioChannelStats = new StatList("AudioChannels", Parser.MediaFiles, f => f.AudioChannels);
+            StatList relGroupStats = new StatList("ReleaseGroup", Parser.MediaFiles, f => f.ReleaseGroup);
 
-            //// PRINT STATS
+            // Print stats
             extStats.Print();
+            yearStats.Print();
+            formatStats.Print();
+            qualityStats.Print();
+            videoRangeStats.Print();
+            videoCodecStats.Print();
+            audioCodecStats.Print();
+            audioChannelStats.Print();
+            relGroupStats.Print();
 
             // Finish and print time taken
             Console.WriteLine("");
