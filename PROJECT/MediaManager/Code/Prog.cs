3db95514b0c615d95b4f4b0cd8fe9f09728be747
David C <45155292+DavoDC@users.noreply.github.com>
2025-03-30 23:10:50 +0800

    Improve 1080p movie handling further

--- PROJECT/MediaManager/Code/Prog.cs
+++ PROJECT/MediaManager/Code/Prog.cs
@@ -85,17 +85,6 @@ namespace MediaManager
                 // 4) Analyse metadata and print statistics
                 Analyser a = new Analyser();
 
-                // TEST
-                Console.WriteLine("");
-                foreach (MediaFile curFile in Parser.MediaFiles)
-                {
-                    if (curFile.AudioCodec.Equals("Unknown"))
-                    {
-                        curFile.PrintAllProperties();
-                        Console.WriteLine("");
-                    }
-                }
-
                 // Print total time
                 //TimeSpan totalTime = ac.ExecutionTime + refl.ExecutionTime + p.ExecutionTime + a.ExecutionTime;
                 //Console.WriteLine("\nTotal time taken: " + Doer.ConvertTimeSpanToString(totalTime));
