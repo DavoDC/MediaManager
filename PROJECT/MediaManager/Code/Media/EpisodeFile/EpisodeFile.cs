e89ba244a65aeaf16e65254b4d682a6431998de1
David C <45155292+DavoDC@users.noreply.github.com>
2025-03-30 20:16:36 +0800

    Fix VideoCodec order for shows
    Video Codec always after AudioCodec+AudioChannels! and just before release group!  all shows!

--- PROJECT/MediaManager/Code/Media/EpisodeFile/EpisodeFile.cs
+++ PROJECT/MediaManager/Code/Media/EpisodeFile/EpisodeFile.cs
@@ -20,9 +20,9 @@ namespace MediaManager.Code.Modules
                     (?:\[(?<CustomFormat>[^]\[]*?)\s*(?<QualityTitle>[^]\[]*)\])?\s*
                     (?:\[(?<VideoDynamicRange>HDR|SDR|Dolby Vision|HLG)\])?\s*
                     (?:\[(?<VideoBitDepth>\d+)bit\])?\s*
-                    (?:\[(?<VideoCodec>x264|x265|h264|h265)\])?\s*
                     (?:\[(?<AudioCodec>[^\]\s]+)\s+(?<AudioChannels>[\d.]+)\])?\s*
                     (?:\[(?<AudioLanguages>[^\]]+)\])?\s*
+                    (?:\[(?<VideoCodec>x264|x265|h264|h265)\])?\s*
                     (?:-(?<ReleaseGroup>[^\]]+))?$", RegexOptions.IgnorePatternWhitespace);
 
         /// <summary>
